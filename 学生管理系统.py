186b5f3bdffc1a12a5b8f73d00c2ff75631ad19c
隔壁老王 <1438426721@qq.com>
2026-06-04 00:05:34 +0800

    学生管理系统3

--- 学生管理系统.py
+++ 学生管理系统.py
@@ -1,29 +1,44 @@
-z = []
+students = [{"name":"张三","age":18},{"name":"李四","age":19},{"name":"王五","age":20}]
 def add_student():
-        a = input("请输入姓名")
-        if a == "":
-           print("姓名不能空")
-        elif a in z:
-           print("该学生已存在")
-        else:
-           z.append(a)
-           print("添加成功")
+        name = input("姓名")
+        age = input("年龄")
+        if name == "":
+               print("姓名不能空")        
+        for name in students:
+               print("该学生已存在")
+               break        
+        else:  
+            students.append({"name":name,"age":age})
+            print("添加成功")
 def remove_student():
-       c = input("请输入删除学生的姓名")
-       if c in z:
-           z.remove(c)
+       name = input("请输入删除学生的姓名")
+       for c in students:
+         if name in students:
+           students.remove(c)
+           print("删除成功")
+           break
+         else:
+            print("该学生不存在")
 def show_student():
-       for d in z :
-          print(d)        
+         for d in students :
+            print(f'姓名：{d["name"]}\n年龄:{d["age"]}') 
+def find_student():
+     name = input("请输入姓名")
+     for e in students:     
+         if name in students:
+            print(f'姓名:{e["name"]}\n年龄:{e["age"]}')
+                                
 while True:
-    b = (input("请输入操作：\n1.添加学生\n2.删除学生\n3.查看学生\n4.退出"))
+    b = (input("请输入操作：\n1.添加学生\n2.删除学生\n3.查看学生\n4.查找学生\n5.退出"))
     if b ==  "1": 
          add_student()
     elif b == "2":
          remove_student()
-    elif b == "3":
-         show_student()
+    elif b == "3":          
+            show_student()
     elif b == "4":
+            find_student()
+    elif b == "5":
        print("退出")
        break 
               
