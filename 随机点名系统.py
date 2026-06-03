17722c3ce0c9e6476ad4c10d04903a2c7230c326
隔壁老王 <1438426721@qq.com>
2026-06-03 22:09:34 +0800

    随机点名系统

--- 随机点名系统.py
+++ 随机点名系统.py
@@ -1,7 +1,8 @@
-
+from random import choice
 students = [
     {"name":"张三","age":18},
     {"name":"李四","age":19},
     {"name":"王五","age":20}
 ]
-print(f'当前共有{len(students)}名学生')
+a = choice(students)
+print(f'恭喜{a["name"]}被抽中')
